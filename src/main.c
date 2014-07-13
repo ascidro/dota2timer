56761132a883ab72dd40a62b449f07b964693d28
Nicolás Satragno <nsatragno@gmail.com>
2014-07-13 01:07:31 -0300

    Añadida actionbar con botones.

--- src/main.c
+++ src/main.c
@@ -3,12 +3,23 @@
 // Constantes.
 #define TIME_BUFFER_SIZE 6
 
-// Recursos: ventana.
+// RECURSOS DE UI:
+// Ventana principal.
 static Window *window;
 
 // Layer principal del texto, muestra el tiempo que transcurrió.
 static TextLayer *main_text;
 
+// Barra que muestra qué hace cada botón.
+static ActionBarLayer *action_bar;
+
+// Botones de la barra de acción.
+static GBitmap* button_image_pause;
+static GBitmap* button_image_roshan;
+static GBitmap* button_image_start;
+static GBitmap* button_image_stop;
+
+// VARIABLES:
 // Búfer de texto para el tiempo transcurrido.
 static char buffer[TIME_BUFFER_SIZE];
 
@@ -32,7 +43,7 @@ int seconds() {
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   if (!started)
     return;
-    
+
   elapsed_time = seconds() - start_time;
   int minutes = elapsed_time / 60;
   int seconds = elapsed_time % 60;
@@ -46,14 +57,19 @@ static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   if (!started)
     return;
   start_time = seconds();
+
+  // Aparecen los iconos de pausa / roshan.
+  action_bar_layer_set_icon(action_bar, BUTTON_ID_UP, button_image_pause);
+  action_bar_layer_set_icon(action_bar, BUTTON_ID_DOWN, button_image_roshan);
 }
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(main_text, "Up");
+  //text_layer_set_text(main_text, "Up");
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(main_text, "Down");
+  //text_layer_set_text(main_text, "Down");
+
 }
 
 static void click_config_provider(void *context) {
@@ -67,12 +83,19 @@ static void window_load(Window *window) {
   GRect bounds = layer_get_bounds(window_layer);
 
   main_text = text_layer_create(
-      (GRect) { .origin = { 0, 10 }, .size = { bounds.size.w, 60 } });
-  text_layer_set_text(main_text, "Press SELECT to start");
+      (GRect) { .origin = { 0, 3 }, .size = { bounds.size.w, 60 } });
+  text_layer_set_text(main_text, "START");
   text_layer_set_text_alignment(main_text, GTextAlignmentCenter);
   text_layer_set_font(main_text,
       fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   layer_add_child(window_layer, text_layer_get_layer(main_text));
+
+  action_bar = action_bar_layer_create();
+  action_bar_layer_add_to_window(action_bar, window);
+  // Al principio solo muestro el icono de start.
+  action_bar_layer_set_icon(action_bar, BUTTON_ID_SELECT, button_image_start);
+  // Registro los botones.
+  action_bar_layer_set_click_config_provider(action_bar, click_config_provider);
 }
 
 static void window_unload(Window *window) {
@@ -80,13 +103,19 @@ static void window_unload(Window *window) {
 }
 
 static void init(void) {
+  // Cargo los bitmaps.
+  button_image_pause = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BUTTON_PAUSE);
+  button_image_roshan = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BUTTON_ROSHAN);
+  button_image_start = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BUTTON_START);
+  button_image_stop = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BUTTON_STOP);
+
   window = window_create();
-  window_set_click_config_provider(window, click_config_provider);
   window_set_window_handlers(window, (WindowHandlers) {
     .load = window_load,
     .unload = window_unload,
   });
-  // TODO investigar qué hace la siguiente línea.
+
+  // Animar la entrada a la aplicación.
   const bool animated = true;
   // Al principio, no estamos ejecutando.
   started = false;
@@ -96,6 +125,7 @@ static void init(void) {
 }
 
 static void deinit(void) {
+  action_bar_layer_destroy(action_bar);
   window_destroy(window);
 }
 
